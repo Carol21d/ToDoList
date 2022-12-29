f27f10b1788aeb7a1b9cb1b9733c2d488a31aa22
blake S <design.harls@icloud.com>
2022-12-29 19:24:36 +0100

    function edit added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 const selecRow = null;
-
+//show message
 function showMessage(message, className) {
   const div = document.createElement("div");
   div.className = `alert alert-${className}`;
@@ -12,8 +12,15 @@ function showMessage(message, className) {
   setTimeout(() => document.querySelector(".alert").remove(), 3000);
 }
 
-document.querySelector("#addItem").value = " ";
 
+//clear fields
+// function clear(){
+//   document.querySelector("#addItem").value = " ";
+// }
+
+
+document.querySelector("#addItem").value = " ";
+// add item
 document.querySelector("#form-ToDo").addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -28,20 +35,35 @@ document.querySelector("#form-ToDo").addEventListener("submit", (e) => {
 
       rowItems.innerHTML = `
                  <td>${addItem}</td>
-                
                  <td>
                  <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
                  <a href="#" class="btn btn-info btn-sm edit">Edit</a>
                 `;
-                listItems.appendChild(rowItems);
-                selecRow = null;
-                showMessage("added item", "success")
-
-    
+      listItems.appendChild(rowItems);
+      selecRow = null;
+      showMessage("Item added", "success");
+    } 
+    else{
+      selecRow.children[0].textContent = addItem;
+      selecRow = null;
+      showMessage("Item Edited", "info");
     }
+    clear();
   }
 });
 
+
+//edit item
+document.querySelector("#items").addEventListener("click", (e) =>{
+  target = e.target;
+  if(target.classList.contains("edit")){
+     selecRow = target.parentElement.parentElement;
+     document.querySelector("#addItem").value = selecRow.children[0].textContent;
+  }
+});
+
+
+//delete
 document.querySelector("#items").addEventListener("click", (e) => {
   target = e.target;
   if (target.classList.contains("delete")) {
